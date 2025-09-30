e5e53173a0c0e2123f2a331548918eae88bb33ee
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-09-30 12:01:39 +0530

    fix: env changed

---
+++ src/app/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseurl: 'http://192.168.1.10/ManageNrich/api/v1'
+};
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseurl:'http://192.168.1.10/ManageNrich/api/v1'
+    baseurl:'https://localhost:7183/api/v1'
 }
